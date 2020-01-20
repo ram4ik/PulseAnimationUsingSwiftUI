5c4cdd0b710eaaf60a685f8fd943759c37b00706
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-01-20 14:03:24 +0200

    screenshot

--- PulseAnimationUsingSwiftUI/ContentView.swift
+++ PulseAnimationUsingSwiftUI/ContentView.swift
@@ -10,12 +10,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Loading()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Loading: View {
+    @State private var animate = false
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.purple.opacity(0.25))
+                .frame(width: 350, height: 350)
+                .scaleEffect(self.animate ? 1 : 0)
+            
+            Circle()
+                .fill(Color.purple.opacity(0.55))
+                .frame(width: 250, height: 250)
+                .scaleEffect(self.animate ? 1 : 0)
+            
+            Circle()
+                .fill(Color.purple.opacity(0.75))
+                .frame(width: 150, height: 150)
+                .scaleEffect(self.animate ? 1 : 0)
+            
+            Circle()
+                .frame(width: 50, height: 50)
+                .scaleEffect(self.animate ? 1 : 0)
+        }.onAppear {
+            self.animate.toggle()
+        }.animation(Animation.linear(duration: 1.5).repeatForever(autoreverses: false))
     }
 }
